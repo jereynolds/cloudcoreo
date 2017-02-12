029f6a4ef955b45f7b48af720731ad06bbd25012
Jacob Reynolds <jacob.e.reynolds@gmail.com>
2017-02-11 23:18:36 -0800

    Reduce the state matrix when its members get large. This somewhat increases the max length of e which can be calculated

--- lib/euler_calc.rb
+++ lib/euler_calc.rb
@@ -8,6 +8,8 @@ class EulerCalc
   @@iteration = 1
   @@state = Matrix.I(2)
   @@state_lock = Mutex.new
+  @@twos_removed = 0
+  @@fives_removed = 0
 
   # Generate the next n digits of e
   def generate(n)
@@ -60,6 +62,8 @@ class EulerCalc
     @@state_lock.synchronize do
       @@state = @@state * transformation(@@iteration)
 
+      reduce_state if @@iteration % 10 == 0
+
       lower = apply(LOWER_BOUND).floor
       upper = apply(UPPER_BOUND).floor
 
@@ -67,6 +71,20 @@ class EulerCalc
 
       @@iteration += 1
     end
+  end
+
+  def reduce_state
+    total_fives = Integer((@@iteration + 1) / 5)
+    total_twos = Integer((@@iteration + 1) / 2)
+
+    twos_to_remove = total_twos - @@twos_removed
+    fives_to_remove = total_fives - @@fives_removed
+    factor_to_remove = (2 ** twos_to_remove) * (5 ** fives_to_remove)
+
+    @@twos_removed += twos_to_remove
+    @@fives_removed += fives_to_remove
+
+    @@state = @@state / Matrix.scalar(2, factor_to_remove)
   end
 
   # Store the digit, then apply a transformation to the state
@@ -96,6 +114,9 @@ class EulerCalc
   def apply(value)
     t = @@state
 
-    Float(t[0, 0] * value + t[0, 1]) / Float(t[1, 0] * value + t[1, 1])
+    numerator = Float(t[0, 0] * value + t[0, 1])
+    denominator = Float(t[1, 0] * value + t[1, 1])
+
+    numerator / denominator
   end
 end
