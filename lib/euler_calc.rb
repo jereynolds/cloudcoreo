d21234855c852e4dd922d6e41f714d49a09af846
Jacob Reynolds <jacob.e.reynolds@gmail.com>
2017-02-11 21:23:23 -0800

    Use ruby's Matrix class instead of using arrays

--- lib/euler_calc.rb
+++ lib/euler_calc.rb
@@ -1,12 +1,12 @@
+require 'matrix'
+
 class EulerCalc
   LOWER_BOUND = 1
   UPPER_BOUND = 2
-  IDENTITY_MATRIX = [1, 0,
-                     0, 1]
 
   @@digits = []
   @@iteration = 1
-  @@state = IDENTITY_MATRIX
+  @@state = Matrix.I(2)
   @@state_lock = Mutex.new
 
   # Generate the next n digits of e
@@ -24,9 +24,7 @@ class EulerCalc
     number = 0
     last = first + length
 
-    if (last > digits.length)
-      generate(last - digits.length)
-    end
+    generate(last - digits.length) if (last > digits.length)
 
     digits[first...last].each do |digit|
       number *= 10
@@ -47,10 +45,10 @@ class EulerCalc
   # bounds converging for the next digit)
   def calc
     @@state_lock.synchronize do
-      @@state = compose(@@state, transformation(@@iteration))
+      @@state = @@state * transformation(@@iteration)
 
-      lower = apply(@@state, LOWER_BOUND).floor
-      upper = apply(@@state, UPPER_BOUND).floor
+      lower = apply(LOWER_BOUND).floor
+      upper = apply(UPPER_BOUND).floor
 
       collect(lower) if lower == upper
 
@@ -61,19 +59,19 @@ class EulerCalc
   # Store the digit, then apply a transformation to the state
   # to subtract the value and multiply by 10
   def collect(digit)
-    transform = [10, -10 * digit,
-                 0,   1]
+    transform = Matrix[ [10, -10 * digit],
+                        [0, 1] ]
 
     digit += 1 if digits.empty? # Algorithm produces e-1 so add 1
     digits.push digit
 
-    @@state = compose(transform, @@state)
+    @@state = transform * @@state
   end
 
   # Gives the transformation matrix for the nth sub-function in the taylor series for e
   def transformation(n)
-    [1, 1+n,
-     0, 1+n]
+    Matrix[ [1, 1 + n],
+            [0, 1 + n] ]
   end
 
   # Apply the mobius transformation to a value representing the tail of the
@@ -82,18 +80,9 @@ class EulerCalc
   # transform is a 2x2 matrix [a, b,
   #                            c, d]
   # value is a scalar
-  def apply(transform, value)
-    a, b, c, d = transform[0..4]
+  def apply(value)
+    t = @@state
 
-    Float(a * value + b) / Float(c * value + d)
-  end
-
-  # Compose two functions by multiplying their matrix representations
-  def compose(m1, m2)
-    a, b, c, d = m1[0..4]
-    w, x, y, z = m2[0..4]
-
-    [a*w + b*y, a*x + b*z,
-     c*w + d*y, c*x + d*z]
+    Float(t[0, 0] * value + t[0, 1]) / Float(t[1, 0] * value + t[1, 1])
   end
 end
